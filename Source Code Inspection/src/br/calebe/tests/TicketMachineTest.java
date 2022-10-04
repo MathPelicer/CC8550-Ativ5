43ca872f42689b0ade9a94578f3c630a78050d5c
MathPelicer <matheuspelicer58@gmail.com>
2022-10-04 16:41:53 -0300

    [code] few new tests at TicketMachineTests

--- Source Code Inspection/src/br/calebe/tests/TicketMachineTest.java
+++ Source Code Inspection/src/br/calebe/tests/TicketMachineTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import ticketmachine.core.TicketMachine;
+import ticketmachine.exception.PapelMoedaInvalidaException;
 
 public class TicketMachineTest {
 
@@ -13,5 +14,56 @@ public class TicketMachineTest {
 
         TicketMachine ticketMachine = new TicketMachine(ticketPrice);
         Assert.assertNotNull(ticketMachine);
+    }
+
+    @Test
+    public void checkInsertBankNoteSuccess() {
+        int valor = 10;
+
+        TicketMachine ticketMachine = new TicketMachine(valor);
+
+        try {
+            ticketMachine.inserir(20);
+        } catch (Exception e) {
+
+        }
+
+        int saldoExpected = 20;
+
+        Assert.assertEquals(saldoExpected, ticketMachine.getSaldo());
+    }
+
+    @Test
+    public void checkInsertBankNoteException() {
+        int valor = 10;
+        String message = "";
+        TicketMachine ticketMachine = new TicketMachine(valor);
+
+        try {
+            ticketMachine.inserir(1);
+        } catch (PapelMoedaInvalidaException e) {
+            message = e.getMessage();
+        }
+
+        Assert.assertEquals(message, "Papel moeda inválido");
+    }
+
+    @Test
+    public void checkPrintSuccess() {
+        int valor = 10;
+        TicketMachine ticketMachine = new TicketMachine(valor);
+        String printTicketMachine = "";
+        try {
+            ticketMachine.inserir(30);
+            printTicketMachine = ticketMachine.imprimir();
+        } catch (Exception e) {
+
+        }
+
+        String result = "*****************\n";
+        result += "*** R$ " + "20,00 ****\n";
+        result += "*****************\n";
+
+        Assert.assertEquals(result, printTicketMachine);
     }
 }
